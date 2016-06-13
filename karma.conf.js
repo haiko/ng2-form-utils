667f90703e4131b5b14855b20c30df45626f0652
haiko <hvdschaaf@gmail.com>
2016-06-14 00:40:25 +0200

    testen aan de gang krijgen

--- karma.conf.js
+++ karma.conf.js
@@ -69,9 +69,9 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS'],
+        browsers: ['Chrome'],
 
-        // Continuous Integration mode
+        // Continuous Integration PhantomJSmode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: true,
 
